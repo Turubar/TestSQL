b1b6869936bb3321c23906e2ff05d4e77a27b81a
Ivan <ivan.turubar@gmail.com>
2023-07-31 00:06:07 +0300

    Тестовое задание

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace myApp
+﻿using System.Data;
+using System.Globalization;
+using System.Text;
+
+namespace myApp
 {
     internal class Program
     {
@@ -19,12 +23,15 @@
                 {
                     // Получаем строку из файла и проверяем подключение,
                     // при неудачном подключении удаляем файл со строкой
-                    if (!ServiceClass.ConnectDB(File.ReadAllText(path)))
+                    string connection = File.ReadAllText(path);
+
+                    if (!ServiceClass.ConnectDB(connection))
                     {
                         Console.WriteLine("Не удалось установить соединение!");
                         file.Delete();
                         Environment.Exit(1);
                     }
+                    else ServiceClass.connectionString = connection;
                 }
                 else
                 {
@@ -49,26 +56,91 @@
                 }
                 #endregion
 
+                // Проверяем корректность параметров. Только 2 пункт принимает больше 1 аргумента (4)
+                if (args[0] == "2" && args.Length != 4) throw new ArgumentException(nameof(args));
+
                 switch (args[0])
                 {
-                    // 1. Создание таблицы Person {}
+                    // 1. Создание таблицы Person {Id int primary key, Fullname: nvarchar(50), Date_birthday: date, Gender: nvarchar(6)}
+                    // Пример входных данных: myApp 1
                     case "1":
+                        // Вызываем метод, который создает таблицу Person и возвращает bool-значение в зависимости от результата
+                        if (ServiceClass.CreateTable()) Console.WriteLine("Таблица Person успешно создана!");
+                        else throw new Exception("Не удалось создать таблицу!");
+                        break;
 
-                        break;
+                    // 2. Добавление записи в таблицу Person. 
+                    //    Пример входных данных: myApp 2 Ivan 01.01.2000 Male
+                    //                         : myApp 2 Maria 06.06.2001 Female
                     case "2":
+                        // Проверяем корректность параметров
+                        if (args[1].Length > 50) throw new ArgumentException(nameof(args));
+                        if (!DateTime.TryParse(args[2], out DateTime date)) throw new ArgumentException(nameof(args));
+                        if (args[3] != "Male" && args[3] != "Female") throw new ArgumentException(nameof(args));
+
+                        // Вызываем метод, который добавляет запись и возвращает bool-значение
+                        if (ServiceClass.InsertRecord(args[1], date.ToString("yyyy-MM-dd"), args[3])) Console.WriteLine("Запись добавлена!");
+                        else throw new Exception("Не удалось добавить запись!");
                         break;
+
+                    // 3. Вывод записей с уникальным значением полей (Fullname, Date_birthday), отсортированных по Fullname,
+                    // вывести Fullname, Date_birthday, Gender, Age (кол-во полных лет).
+                    // Пример входных данных: myApp 3
                     case "3":
+                        // Вызываем метод, который выполняет запрос на выборку данных и возвращает объект DataTable
+                        var data = ServiceClass.SelectUniqueRecords();
+                        if (data != null)
+                        {
+                            // Выводим данные в консоль
+                            Console.WriteLine("{0, -30} {1, -20} {2, -10} {3, 0}", "ФИО", "Дата рождения", " " + "Пол", "Возраст");
+
+                            foreach (DataRow row in data.Rows) 
+                                Console.WriteLine("{0, -30} {1, -20} {2, -10} {3, 0}", row[0], " " + ((DateTime) row[1]).ToShortDateString(), row[2], "  " + row[3]);
+                        }
+                        else throw new NullReferenceException(nameof(data));
                         break;
+
+                    // 4. Вставка миллиона записей...
+                    // Пример входных данных: myApp 4
                     case "4":
+                        Console.WriteLine("Загрузка...");
+
+                        // Вызываем метод, который выполняет запрос на добавление ~ 1 млн. записей...
+                        if (ServiceClass.InsertMillionRecords()) Console.WriteLine("Добавление строк успешно завершено!");
+                        else throw new Exception("Что-то полшло не так!");
                         break;
+
+                    // 5. Выборка по критерию (WHERE Fullname LIKE 'F%' AND Gender = 'Male')
                     case "5":
+                        // Вызываем метод, который выполняет запрос на выборку данных и возвращает объект DataTable
+                        var tuple = ServiceClass.SelectLikeRecords();
+
+                        if (tuple.table != null)
+                        {
+                            // Вывод данных в консоль
+                            //foreach (DataRow row in tuple.table.Rows)
+                            //    Console.WriteLine("{0, -10} {1, -30} {2, -20} {3, 0}", row[0], row[1], " " + ((DateTime)row[2]).ToShortDateString(), row[3]);
+
+                            Console.WriteLine($"~ Время выполнения запроса: {tuple.time} ms; Количество строк: {tuple.table.Rows.Count}");
+                        }
+                        else throw new NullReferenceException(nameof(tuple));
                         break;
+
+                    // 6. Оптимизация запроса с помощью создания индекса
                     case "6":
+                        // Вызываем метод, который создает/удаляет индекс
+                        string? result = ServiceClass.OptimizeQuery();
+
+                        if (result != null)
+                        {
+                            if (result == "Create") Console.WriteLine("Индекс успешно создан!");
+                            else Console.WriteLine("Индекс удален!");
+                        }
+                        else throw new NullReferenceException(nameof(result));
                         break;
                     default:
                         throw new ArgumentException(nameof(args));
                 }
-
             }
             // Выбрасываем exсeption, т.к. отсутствует параметр командной строки 
             else throw new ArgumentNullException(nameof(args));
--- myApp/ServiceClass.cs
+++ myApp/ServiceClass.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlTypes;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
+using System.Diagnostics;
 
 namespace myApp
 {
@@ -20,11 +24,50 @@ namespace myApp
                 try
                 {
                     var connection = new SqlConnection(connectionString);
-
                     connection.Open();
 
-                    if (connection.State == System.Data.ConnectionState.Open)
+                    if (connection.State == ConnectionState.Open)
                     {
+                        connection.Close();
+                        return true;
+                    }
+                    else return false;
+                }
+                catch
+                {
+                    return false;
+                }
+            } 
+            else return false;
+        }
+
+        // Создание таблицы Person
+        public static bool CreateTable()
+        {
+            if (connectionString != null)
+            {
+                try
+                {
+                    var connection = new SqlConnection(connectionString);
+                    connection.Open();
+
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        var query = new SqlCommand();
+                        query.Connection = connection;
+
+                        query.CommandText = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'Person'";
+                        var reader = query.ExecuteReader();
+
+                        string createPerson = "CREATE TABLE Person (Id int primary key identity, Fullname nvarchar(50), Date_birthday date, Gender nvarchar(6))";
+
+                        if (reader.HasRows) query.CommandText = "DROP TABLE Person;" + createPerson;
+                        else query.CommandText = createPerson;
+
+                        reader.Close();
+
+                        query.ExecuteNonQuery();
+
                         connection.Close();
                         return true;
                     }
@@ -36,6 +79,215 @@ namespace myApp
                 }
             }
             else return false;
+        }
+
+        // Добавление записи в таблицу Person
+        public static bool InsertRecord(string fullname, string date, string gender)
+        {
+            if (connectionString != null)
+            {
+                try
+                {
+                    var connection = new SqlConnection(connectionString);
+                    connection.Open();
+
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        var query = new SqlCommand($"INSERT INTO Person VALUES ('{fullname}', '{date}', '{gender}')", connection);
+                        query.ExecuteNonQuery();
+
+                        connection.Close();
+                        return true;
+                    }
+                    else return false;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            else return false;
+        }
+
+        // Выборка уникальных значений по (Fullname + Date_birthday), отсорт. по Fullname
+        public static DataTable? SelectUniqueRecords()
+        {
+            if (connectionString != null)
+            {
+                try
+                {
+                    var connection = new SqlConnection(connectionString);
+                    connection.Open();
+
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        var query = new SqlCommand($"SELECT p.Fullname AS 'ФИО', p.Date_birthday AS 'Дата рождения', p.Gender AS 'Пол', " +
+                        $"DATEDIFF(yy, p.Date_birthday, GETDATE()) - CASE WHEN (MONTH(p.Date_birthday) > MONTH(GETDATE())) OR (MONTH(p.Date_birthday) = MONTH(GETDATE()) AND DAY(p.Date_birthday) > DAY(GETDATE())) THEN 1 ELSE 0 END AS 'Возраст'" +
+                        $"FROM (SELECT  *, ROW_NUMBER() OVER(PARTITION BY Fullname, Date_birthday ORDER BY Fullname) rn FROM Person) p WHERE rn = 1 ORDER BY Fullname", connection);
+
+                        SqlDataAdapter adapter = new SqlDataAdapter(query);
+                        DataSet data = new DataSet();
+                        adapter.Fill(data);
+
+                        connection.Close();
+
+                        return data.Tables[0];
+                    }
+                    else return null;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            else return null;
+        }
+
+        // Вставка ~ миллиона записей в таблицу Person
+        public static bool InsertMillionRecords()
+        {
+            if (connectionString != null)
+            {
+                try
+                {
+                    var connection = new SqlConnection(connectionString);
+
+                    var adapter = new SqlDataAdapter("SELECT * FROM Person", connection);
+                    var data = new DataTable();
+                    adapter.FillSchema(data, SchemaType.Source);
+
+                    string[] alphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                                          "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+                    Random ran = new Random();
+
+                    for (int i = 0; i < 1_000_000; i += alphabet.Length)
+                    {
+                        for (int j = 0; j < alphabet.Length; j++)
+                        {
+                            var row = data.NewRow();
+
+                            row["Fullname"] = alphabet[j];
+                            row["Date_birthday"] = DateTime.Now.ToString("yyyy-MM-dd");
+                            row["Gender"] = ran.Next(0, 2) == 0 ? "Male" : "Female";
+
+                            data.Rows.Add(row);
+                        }
+                    }
+
+                    string[] names = { "Fedor", "Fabricio", "Fred", "Fidel", "Florence", "Frank", "Felix" };
+
+                    for(int i = 0; i < 100; i++)
+                    {
+                        var row = data.NewRow();
+
+                        row["Fullname"] = names[ran.Next(0, names.Length)];
+                        row["Date_birthday"] = DateTime.Now.ToString("yyyy-MM-dd");
+                        row["Gender"] = "Male";
+
+                        data.Rows.Add(row);
+                    }
+
+                    data.AcceptChanges();
+
+                    var bulk = new SqlBulkCopy(connection) { DestinationTableName = "Person" };
+
+                    connection.Open();
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        bulk.WriteToServer(data);
+
+                        connection.Close();
+                        return true;
+                    }
+                    else return false;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            else return false;
+        }
+
+        // Выборка значений, где Fullname начинается с 'F' (LIKE 'F%') и Gender = 'Male' 
+        public static (DataTable? table, long time) SelectLikeRecords()
+        {
+            if (connectionString != null)
+            {
+                try
+                {
+                    var connection = new SqlConnection(connectionString);
+                    connection.Open();
+
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        var query = new SqlCommand($"SELECT * FROM Person WHERE Fullname LIKE 'F%' AND Gender = 'Male'", connection);
+
+                        SqlDataAdapter adapter = new SqlDataAdapter(query);
+                        DataSet data = new DataSet();
+
+                        var stopwatch = new Stopwatch();
+                        
+                        stopwatch.Start();
+                        adapter.Fill(data);
+                        stopwatch.Stop();
+
+                        connection.Close();
+                        return (data.Tables[0], stopwatch.ElapsedMilliseconds);
+                    }
+                    else return (null, 0);
+                }
+                catch
+                {
+                    return (null, 0);
+                }
+            }
+            else return (null, 0);
+        }
+
+        // Создание/удаление индекса в таблице Person
+        public static string? OptimizeQuery()
+        {
+            if (connectionString != null)
+            {
+                try
+                {
+                    var connection = new SqlConnection(connectionString);
+                    connection.Open();
+
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        var query = new SqlCommand($"SELECT * FROM sys.indexes WHERE name = 'IX_Person'", connection);
+                        SqlDataReader reader = query.ExecuteReader();
+
+                        string result = "";
+                        if (reader.HasRows)
+                        {
+                            query.CommandText = "DROP INDEX IX_Person on Person";
+                            result = "Delete";
+                        }
+                        else
+                        {
+                            query.CommandText = "CREATE INDEX IX_Person ON Person (Fullname, Gender) INCLUDE (Date_birthday) WHERE Gender = 'Male'";
+                            result = "Create";
+                        }
+
+                        reader.Close();
+
+                        query.ExecuteNonQuery();
+
+                        connection.Close();
+                        return result;
+                    }
+                    else return null;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            else return null;
         }
     }
 }
